08638cac32e5c6e62ddee806829081342e79a376
GBAGUIDI-M <113685865+GBAGUIDI-M@users.noreply.github.com>
2026-04-16 23:14:33 +0200

    Update API endpoint to new URL

--- frontend/app/insights/page.tsx
+++ frontend/app/insights/page.tsx
@@ -9,7 +9,7 @@ export default function InsightsPage() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    axios.get("https://underscoreaims.onrender.com/explain").then(res => {
+    axios.get("http://161.35.74.143/explain").then(res => {
       if (res.data && !res.data.message) {
         // convert dict to array
         const sorted = Object.entries(res.data)
