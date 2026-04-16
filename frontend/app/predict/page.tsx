c8f06b65f1457749d0cbb3ae6996e277a02d38d7
GBAGUIDI-M <113685865+GBAGUIDI-M@users.noreply.github.com>
2026-04-16 23:51:37 +0200

    Update prediction API endpoint to use HTTPS

--- frontend/app/predict/page.tsx
+++ frontend/app/predict/page.tsx
@@ -30,7 +30,7 @@ export default function PredictorPage() {
     setResult(null);
 
     try {
-      const res = await axios.post("http://161.35.74.143/predict", {
+      const res = await axios.post("https://cmdj.dpdns.org/predict", {
         home_team: homeTeam,
         away_team: awayTeam
       });
